d333577af4d37c139b9fdf33cf76036937525ad1
Andreyc4d <Sbtr@mail.ru>
2015-09-21 01:13:02 +0300

    Version 0.1.0 hz chto eto znachit  Eto moi pervyi commit

---
+++ src/edu/javacourse/robot/Robot.java
@@ -0,0 +1,46 @@
+package edu.javacourse.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Andrey on 18.09.2015.
+ */
+public class Robot {
+    private double x = 0;
+    private double y = 0;
+    protected double course = 0;
+    private ArrayList<RobotLine> lines = new ArrayList<RobotLine>();
+    public Robot(double x, double y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public void forward (int distance) {
+        x += distance * Math.cos(course / 180 * Math.PI);
+        y = y + distance * Math.sin(course/180 * Math.PI);
+        lines.add(new RobotLine(xOld,yOld,x,y));
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
+    }
+
+    public ArrayList<RobotLine> getLines() {
+        return lines;
+    }
+}
---
+++ src/edu/javacourse/robot/ui/RobotPathComponent.java
@@ -0,0 +1,13 @@
+package edu.javacourse.robot.ui;
+
+import edu.javacourse.robot;
+import edu.javacourse.robot.RobotLine;
+import java.awt.Graphics;
+import java.swing.JComponent;
+
+
+/**
+ * Created by Andrey on 18.09.2015.
+ */
+public class RobotPathComponent {
+}
